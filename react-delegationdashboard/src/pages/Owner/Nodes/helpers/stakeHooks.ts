838d9bd817120b316a502bf3a90ba7aedf29ee1b
Traian Anghel <tanghel@live.com>
2021-03-18 17:03:27 +0200

    somre more removals

--- react-delegationdashboard/src/pages/Owner/Nodes/helpers/stakeHooks.ts
+++ react-delegationdashboard/src/pages/Owner/Nodes/helpers/stakeHooks.ts
@@ -1,33 +1,8 @@
 import { DappState } from 'context/state';
 import { Delegation } from 'contracts';
 import numberToRequestData from 'helpers/converters';
-import { number } from 'yup/lib/locale';
 
 export const nodeTransactions = {
-  unStake: (blsKey: string, dapp: DappState, delegationContract?: string) => {
-    const delegation = new Delegation(dapp.proxy, delegationContract, dapp.provider);
-    return delegation.sendTransaction('0', 'unStakeNodes', blsKey);
-  },
-  reStake: (blsKey: string, dapp: DappState, delegationContract?: string) => {
-    const delegation = new Delegation(dapp.proxy, delegationContract, dapp.provider);
-    return delegation.sendTransaction('0', 'reStakeUnStakedNodes', blsKey);
-  },
-  unJail: (blsKey: string, dapp: DappState, delegationContract?: string) => {
-    const delegation = new Delegation(dapp.proxy, delegationContract, dapp.provider);
-    return delegation.sendTransaction('2.5', 'unJailNodes', blsKey);
-  },
-  unBond: (blsKey: string, dapp: DappState, delegationContract?: string) => {
-    const delegation = new Delegation(dapp.proxy, delegationContract, dapp.provider);
-    return delegation.sendTransaction('0', 'unBondNodes', blsKey);
-  },
-  stake: (blsKey: string, dapp: DappState, delegationContract?: string) => {
-    const delegation = new Delegation(dapp.proxy, delegationContract, dapp.provider);
-    return delegation.sendTransaction('0', 'stakeNodes', `${blsKey}`);
-  },
-  remove: (blsKey: string, dapp: DappState, delegationContract?: string) => {
-    const delegation = new Delegation(dapp.proxy, delegationContract, dapp.provider);
-    return delegation.sendTransaction('0', 'removeNodes', `${blsKey}`);
-  },
   sign: (actionId: number, dapp: DappState, multisigContract?: string) => {
     const delegation = new Delegation(dapp.proxy, multisigContract, dapp.provider);
     return delegation.sendTransaction('0', 'sign', numberToRequestData(actionId));
--- react-delegationdashboard/src/pages/Owner/index.tsx
+++ react-delegationdashboard/src/pages/Owner/index.tsx
@@ -3,7 +3,6 @@ import { Address } from '@elrondnetwork/erdjs/out';
 import React from 'react';
 import { useContext } from 'context';
 import Overview from 'components/Overview';
-import Nodes from './Nodes';
 
 const Owner = () => {
   const { address, contractOverview, loggedIn } = useContext();
@@ -23,7 +22,6 @@ const Owner = () => {
           <div className="card border-0">
             <Overview />
             <div className="card-body pt-0 px-spacer pb-spacer">
-              <Nodes />
             </div>
           </div>
         </div>
