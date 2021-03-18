7e4c13db0ec0165e09d48c0831836e91bf396a09
Traian Anghel <tanghel@live.com>
2021-03-18 16:40:42 +0200

    Wallet access screen improvements

--- react-delegationdashboard/src/pages/Home/Login/Wallet.tsx
+++ react-delegationdashboard/src/pages/Home/Login/Wallet.tsx
@@ -59,7 +59,7 @@ const WalletLogin = () => {
 
   return (
     <button onClick={handleOnClick} className="btn btn-primary px-sm-spacer mx-1 mx-sm-3">
-      Wallet
+      Access wallet
     </button>
   );
 };
--- react-delegationdashboard/src/pages/Home/index.tsx
+++ react-delegationdashboard/src/pages/Home/index.tsx
@@ -30,13 +30,8 @@ const Home = () => {
           <div className="card my-spacer text-center">
             <div className="card-body p-spacer mx-lg-spacer">
               <Logo className="logo mb-spacer" />
-              <h4 className="mb-spacer">Elrond Delegation Manager</h4>
-              <p className="lead mb-spacer">
-                Delegate Elrond ({egldLabel}) and earn up to 25% APY!
-              </p>
-              <p className="mb-spacer">Please select your login method:</p>
+              <h4 className="mb-spacer">Elrond Multisig Manager</h4>
               <div>
-                <LedgerLogin />
                 <WalletLogin />
               </div>
             </div>
