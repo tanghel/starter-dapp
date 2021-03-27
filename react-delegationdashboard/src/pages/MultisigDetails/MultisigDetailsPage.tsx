61c30436c2edbec26c3f8ae20fb5d2b06e2a2033
Traian Anghel <tanghel@live.com>
2021-03-27 11:13:46 +0200

    merged some components into MultisigDetailsPage directly

--- react-delegationdashboard/src/pages/MultisigDetails/MultisigDetailsPage.tsx
+++ react-delegationdashboard/src/pages/MultisigDetails/MultisigDetailsPage.tsx
@@ -1,22 +1,75 @@
 import React from 'react';
 import { useContext } from 'context';
 import Multisig from './Multisig';
-import { Redirect } from 'react-router-dom';
-import MultisigDetailsHeader from 'pages/MultisigDetails/MultisigDetailsHeader';
-import MultisigDetailsCards from 'pages/MultisigDetails/MultisigDetailsCards';
+import { Link, Redirect } from 'react-router-dom';
+import StatCard from 'components/StatCard';
 
 const MultisigDetailsPage = () => {
-  const { currentMultisigAddress } = useContext();
+  const { currentMultisigAddress, totalBoardMembers, totalProposers, quorumSize, userRole } = useContext();
 
   if (!currentMultisigAddress) {
     return <Redirect to="/owner" />;
   }
 
+  const userRoleAsString = () => {
+    switch (userRole) {
+      case 0:
+        return 'No rights';
+      case 1:
+        return 'Proposer';
+      case 2:
+        return 'Proposer/Signer';
+      default:
+        return 'Unknown';
+    }
+  };
+
   return (
     <div className="dashboard w-100">
       <div className="card border-0">
-        <MultisigDetailsHeader />
-        <MultisigDetailsCards />
+        <div className="header card-header d-flex align-items-center border-0 justify-content-between px-spacer">
+          <div className="py-spacer text-truncate">
+            <p className="opacity-6 mb-0">Multisig Address</p>
+            <span className="text-truncate">{currentMultisigAddress?.bech32()}</span>
+          </div>
+          <div className="d-flex justify-content-center align-items-center justify-content-between">
+            <Link to="/owner" className="btn btn-primary btn-sm">
+              Manage multisigs
+            </Link>
+          </div>
+        </div>
+        
+
+        <div className="cards d-flex flex-wrap mr-spacer">
+          <StatCard
+            title="Board Members"
+            value={totalBoardMembers.toString()}
+            color="orange"
+            svg="contract.svg"
+          />
+          <StatCard
+            title="Proposers"
+            value={totalProposers.toString()}
+            valueUnit=""
+            color="purple"
+            svg="nodes.svg"
+          />
+          <StatCard
+            title="Quorum size"
+            value={quorumSize.toString()}
+            valueUnit=""
+            color="orange"
+            svg="leaf-solid.svg"
+          />
+          <StatCard
+            title="User role"
+            value={userRoleAsString()}
+            valueUnit=""
+            color="orange"
+            svg="leaf-solid.svg"
+          />
+        </div>
+
         <div className="card-body pt-0 px-spacer pb-spacer">
           <Multisig />
         </div>
